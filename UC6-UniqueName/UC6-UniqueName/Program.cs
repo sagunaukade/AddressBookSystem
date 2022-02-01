f1ee264fc8a041e9ce4cfee2dd50fc66165451bf
sagunaukade <53419654+sagunaukade@users.noreply.github.com>
2022-02-02 00:04:36 +0530

    Adding multiple Address Book to the System Each Address Book has a unique Name

---
+++ UC6-UniqueName/UC6-UniqueName/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UC6_UniqueName
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Address Book Progam \n");
+            CreateAddressBook createAddressBook = new CreateAddressBook();
+            createAddressBook.ReadInput();
+            Console.ReadLine();
+        }
+    }
+    
+}
