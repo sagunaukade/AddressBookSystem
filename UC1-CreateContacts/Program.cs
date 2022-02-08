220465ead1b4a3fb7c08a7e535d3a4701c42a8b7
sagunaukade <53419654+sagunaukade@users.noreply.github.com>
2022-02-08 16:07:40 +0530

    Adding program to the create contacts

---
+++ UC1-CreateContacts/Program.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UC1_CreateContacts
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the Address book system");
+            Console.ReadLine();
+            AddressBook.PersonDetails();
+        }
+    }
+}
