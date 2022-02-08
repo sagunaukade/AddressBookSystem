51a0eff82cbbbb84200927760d745cfc80298737
sagunaukade <53419654+sagunaukade@users.noreply.github.com>
2022-02-08 15:57:49 +0530

    Adding program to the delete contacts

---
+++ UC4-DeleteContacts/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UC4_DeleteContacts
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the Address Book System");
+            Console.ReadLine();
+            UC4_DeleteContacts.AddressBook.RemovePeople();
+            UC4_DeleteContacts.AddressBook.GetCustomer();
+            UC4_DeleteContacts.AddressBook.ListingPeople();
+            //UC4_DeleteContacts.AddressBook.PrintCustomer();
+
+        }
+    }
+}
