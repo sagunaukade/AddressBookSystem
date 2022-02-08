c3701ed3b6931cff3133c47a30c681b07e2e1711
sagunaukade <53419654+sagunaukade@users.noreply.github.com>
2022-02-08 17:09:16 +0530

    Adding to the adding new contact

---
+++ UC2-AddingNewContacts/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UC2_AddingNewContacts
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the Address Book System");
+            Console.ReadLine();
+            UC2_AddingNewContacts.AddressBook.GetPerson();
+
+            UC2_AddingNewContacts.AddressBook.ListingPeople();
+
+        }
+    }
+}
