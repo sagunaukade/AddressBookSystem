1f43ce8d7eef151444346dba8bc0684fa5a2d0f1
sagunaukade <53419654+sagunaukade@users.noreply.github.com>
2022-03-07 17:35:04 +0530

    [Saguna]Add to Read/Write the Address Book  with Persons Contact  as CSV File

---
+++ AddressBookSystem/Contact.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddressBookSystem
+{
+    public class Contact
+    {
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+        public string address { get; set; }
+        public string city { get; set; }
+        public string state { get; set; }
+        public int zipcode { get; set; }
+        public long phoneNumber { get; set; }
+        public string email { get; set; }
+
+        public Contact(string firstName, string lastName, string address, string city, string state, int zipcode, long phoneNumber, string email)
+        {
+            this.firstName = firstName;
+            this.lastName = lastName;
+            this.address = address;
+            this.city = city;
+            this.state = state;
+            this.zipcode = zipcode;
+            this.phoneNumber = phoneNumber;
+            this.email = email;
+        }
+        public override string ToString()
+        {
+            return $"{firstName} {lastName} {address} {city} {state} {zipcode} {phoneNumber} {email}";
+        }
+    }
+}
+
